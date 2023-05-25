1f2073712b19a4368892caabadf47e615c2fa7bb
fernandoap46 <fernandoap46@gmail.com>
2023-05-25 16:51:01 -0300

    ajuste css

---
+++ paises/src/App.jsx
@@ -0,0 +1,26 @@
+
+
+import './App.css'
+import Body from './component/Body'
+import MenuBar from './component/MenuBar'
+
+
+import React, { useState } from "react";
+
+
+function App() {
+ 
+ 
+
+  return (
+    <div className='app'>
+      
+      <MenuBar/>
+      <Body/>
+    </div>
+     
+    
+  )
+}
+
+export default App
