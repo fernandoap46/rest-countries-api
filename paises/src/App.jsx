39064e7e9435482664aa003137921e6aa3a77bd8
fernandoap46 <fernandoap46@gmail.com>
2023-05-29 22:39:22 -0300

    iniciando testes utilizando jest

--- paises/src/App.jsx
+++ paises/src/App.jsx
@@ -1,26 +1,16 @@
+import './App.css';
+import Body from "./component/Body";
+import MenuBar from "./component/MenuBar";
 
-
-import './App.css'
-import Body from './component/Body'
-import MenuBar from './component/MenuBar'
-
-
-import React, { useState } from "react";
-
+import React from "react";
 
 function App() {
- 
- 
-
   return (
-    <div className='app'>
-      
-      <MenuBar/>
-      <Body/>
+    <div className="app">
+      <MenuBar />
+      <Body />
     </div>
-     
-    
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ paises/src/App.spec.jsx
@@ -0,0 +1,9 @@
+import{render,screen} from '@testing-library/react'
+
+
+describe('jest', () => {
+    it('should work', ()=> {
+        expect(1).toBe(1)
+    })
+    
+})
